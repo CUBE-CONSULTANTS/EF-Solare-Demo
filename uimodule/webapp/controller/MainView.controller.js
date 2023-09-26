b54bd417a70175be791da2d061d5508364709dab
salvatorelaspata <salvatore.la.spata@gmail.com>
2023-09-26 16:05:44 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/myorg/emptyStaticProject/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.myorg.emptyStaticProject.controller.MainView", {});
+});
